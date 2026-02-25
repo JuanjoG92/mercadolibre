9570a7d30a70fa70f5e8445f9b34280bdb3da04a
TecnoCenter Sistemas <tecnocentersistemas@gmail.com>
2026-02-25 16:43:45 -0300

    PWA: manifest.json, service worker, iconos SVG y meta tags en todos los HTML

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'ecomarket-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/register.html',
+  '/publish.html',
+  '/favorites.html',
+  '/my-products.html',
+  '/css/app.css',
+  '/js/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(res) {
+      var clone = res.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(e.request, clone);
+      });
+      return res;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
